4521305bffe02dce413f656e6f9e1c64c9293314
Marko Koschak <marko.koschak@tisno.de>
2014-08-11 20:03:02 +0000

    Added build_MW_package.sh

---
+++ build_MW_package.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+MER_HYBRIS_DEVEL=devel/mer-hybris
+sudo mkdir -p /parentroot$MER_ROOT/$MER_HYBRIS_DEVEL
+sudo chown -R $USER /parentroot$MER_ROOT/$MER_HYBRIS_DEVEL
+cd /parentroot$MER_ROOT/$MER_HYBRIS_DEVEL
+
