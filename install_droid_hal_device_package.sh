60960a485053b6fcb340ee4e0202594c636deca5
Marko Koschak <marko.koschak@tisno.de>
2014-08-11 21:44:54 +0200

    Update install_droid_hal_device_package.sh

--- install_droid_hal_device_package.sh
+++ install_droid_hal_device_package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Install the droid-hal-device headers into SB2"
-sb2 -t $VENDOR-$DEVICE-armv7hl -R -msdk-install zypper ref
-sb2 -t $VENDOR-$DEVICE-armv7hl -R -msdk-install \
+sb2 -t $VENDOR-$DEVICE-armv7hl -m sdk-install -R zypper ref
+sb2 -t $VENDOR-$DEVICE-armv7hl -m sdk-install -R \
 zypper install droid-hal-$DEVICE-devel
