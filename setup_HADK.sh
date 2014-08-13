c75b87c9026c9e6576c97bc8765a9092e6eea70d
Marko Koschak <marko.koschak@tisno.de>
2014-08-13 10:14:45 +0200

    Added check for running inside Mer SDK

--- setup_HADK.sh
+++ setup_HADK.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-sudo zypper in android-tools createrepo zip
+# This script is supposed to be run in the Mer SDK environment
+if [ -d "/parentroot" ]; then
+  if [ ! -d "/parentroot/parentroot"]; then
+    sudo zypper in android-tools createrepo zip
 
-hadk
-TARBALL=ubuntu-trusty-android-rootfs.tar.bz2
-curl -O http://img.merproject.org/images/mer-hybris/ubu/$TARBALL
-UBUNTU_CHROOT=/parentroot$MER_ROOT/sdks/ubuntu
-sudo mkdir -p $UBUNTU_CHROOT
-sudo tar --numeric-owner -xvjf $TARBALL -C $UBUNTU_CHROOT
+    hadk
+    TARBALL=ubuntu-trusty-android-rootfs.tar.bz2
+    curl -O http://img.merproject.org/images/mer-hybris/ubu/$TARBALL
+    UBUNTU_CHROOT=/parentroot$MER_ROOT/sdks/ubuntu
+    sudo mkdir -p $UBUNTU_CHROOT
+    sudo tar --numeric-owner -xvjf $TARBALL -C $UBUNTU_CHROOT
+  else
+    echo "Error: Don\'t run this script in the HABuildSDK chroot environment"
+    exit 1
+  fi
+else
+  echo "Error: Run this script in the Mer SDK chroot environment"
+  exit 1
+fi
