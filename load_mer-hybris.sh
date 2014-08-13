5b4b021812d61daafc4fe326ee1b266816c76fe1
Marko Koschak <marko.koschak@tisno.de>
2014-08-13 14:05:08 +0200

    Update load_mer-hybris.sh

--- load_mer-hybris.sh
+++ load_mer-hybris.sh
@@ -3,10 +3,9 @@
 # This script is supposed to be run in the HABuildSDK environment
 if [ -d "/parentroot/parentroot"]; then
   hadk
-  LOCAL_ANDROID_ROOT=/parentroot/parentroot$ANDROID_ROOT
-  sudo mkdir -p $LOCAL_ANDROID_ROOT
-  sudo chown -R $USER $LOCAL_ANDROID_ROOT
-  cd $LOCAL_ANDROID_ROOT
+  sudo mkdir -p $ANDROID_ROOT
+  sudo chown -R $USER $ANDROID_ROOT
+  cd $ANDROID_ROOT
   repo init -u git://github.com/mer-hybris/android.git -b hybris-10.1
   repo sync
   source build/envsetup.sh
