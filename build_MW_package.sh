1a7a62226994978d74de5332d480ba6fe8483ccc
Marko Koschak <marko.koschak@tisno.de>
2014-08-13 13:02:51 +0200

    Update build_MW_package.sh

--- build_MW_package.sh
+++ build_MW_package.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+# This script is supposed to be run in the Mer SDK environment
+if [ ! -d "/parentroot" ]; then
+  echo "Error: Run this script in the Mer SDK chroot environment"
+  exit 1
+fi
+if [ -d "/parentroot/parentroot"]; then
+  echo "Error: Don\'t run this script in the HABuildSDK chroot environment"
+  exit 1
+fi
+
 PKG=$1
 
-echo "Building package for $1"
+echo "Building package for $PKG"
 sudo mkdir -p /parentroot$MER_ROOT/devel/mer-hybris
 sudo chown -R $USER /parentroot$MER_ROOT/devel/mer-hybris
 cd /parentroot$MER_ROOT/devel/mer-hybris
