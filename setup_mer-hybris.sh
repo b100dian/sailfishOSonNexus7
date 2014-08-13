3859ed62f2add4099e988e80fd409ea0fe19e725
Marko Koschak <marko.koschak@tisno.de>
2014-08-13 10:20:27 +0200

    Update setup_mer-hybris.sh

--- setup_mer-hybris.sh
+++ setup_mer-hybris.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-hadk
-sudo mkdir -p $ANDROID_ROOT
-sudo chown -R $USER $ANDROID_ROOT
-cd $ANDROID_ROOT
-repo init -u git://github.com/mer-hybris/android.git -b hybris-10.1
-repo sync
-
-cd $ANDROID_ROOT
-hadk
-source build/envsetup.sh
-export USE_CCACHE=1
-breakfast $DEVICE
-rm .repo/local_manifests/roomservice.xml
+# This script is supposed to be run in the HABuildSDK environment
+if [ -d "/parentroot/parentroot"]; then
+  hadk
+  LOCAL_ANDROID_ROOT=/parentroot/parentroot$ANDROID_ROOT
+  sudo mkdir -p $LOCAL_ANDROID_ROOT
+  sudo chown -R $USER $LOCAL_ANDROID_ROOT
+  cd $LOCAL_ANDROID_ROOT
+  repo init -u git://github.com/mer-hybris/android.git -b hybris-10.1
+  repo sync
+  source build/envsetup.sh
+  export USE_CCACHE=1
+  breakfast $DEVICE
+  rm .repo/local_manifests/roomservice.xml
+else
+  echo "Error: Run this script in the HABuildSDK chroot environment"
+  exit 1
+fi
