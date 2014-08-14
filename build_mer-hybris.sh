38944d10b492743de0cdb426010acc9609d75dd3
Marko Koschak <marko.koschak@tisno.de>
2014-08-14 21:40:45 +0200

    Uncommented loading repo

--- build_mer-hybris.sh
+++ build_mer-hybris.sh
@@ -5,8 +5,8 @@ if [ -d "/parentroot/parentroot" ]; then
   sudo mkdir -p $ANDROID_ROOT
   sudo chown -R $USER $ANDROID_ROOT
   cd $ANDROID_ROOT
-#  repo init -u git://github.com/mer-hybris/android.git -b hybris-10.1
-#  repo sync
+  repo init -u git://github.com/mer-hybris/android.git -b hybris-10.1
+  repo sync
   source build/envsetup.sh
   export USE_CCACHE=1
   breakfast $DEVICE
