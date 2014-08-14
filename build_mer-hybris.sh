b3620e2f92ffaefb1db34fc6b046e7ceb04d4b32
Marko Koschak <marko.koschak@tisno.de>
2014-08-14 20:48:55 +0200

    Now building hybris-hal is done in the script

--- build_mer-hybris.sh
+++ build_mer-hybris.sh
@@ -5,12 +5,13 @@ if [ -d "/parentroot/parentroot" ]; then
   sudo mkdir -p $ANDROID_ROOT
   sudo chown -R $USER $ANDROID_ROOT
   cd $ANDROID_ROOT
-  repo init -u git://github.com/mer-hybris/android.git -b hybris-10.1
-  repo sync
+#  repo init -u git://github.com/mer-hybris/android.git -b hybris-10.1
+#  repo sync
   source build/envsetup.sh
   export USE_CCACHE=1
   breakfast $DEVICE
   rm .repo/local_manifests/roomservice.xml
+  make -j4 hybris-hal
 else
   echo "Error: Run this script in the HABuildSDK chroot environment"
   exit 1
