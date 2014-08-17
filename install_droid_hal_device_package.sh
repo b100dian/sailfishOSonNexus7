9ada212e91b5640bd03ecdf6b35fc33a314722c5
Marko Koschak <marko.koschak@tisno.de>
2014-08-17 21:29:12 +0200

    Update SB2 target to always use an up-to-date repo

--- install_droid_hal_device_package.sh
+++ install_droid_hal_device_package.sh
@@ -5,6 +5,10 @@
 # This script is supposed to be run in the Mer SDK environment
 checkMerSDK
 
+echo "Set the SB2 target to use an up-to-date repo"
+sb2 -t $VENDOR-$DEVICE-armv7hl -m sdk-install -R ssu domain sales
+sb2 -t $VENDOR-$DEVICE-armv7hl -m sdk-install -R ssu dr sdk
+
 echo "Install the droid-hal-device headers into SB2"
 sb2 -t $VENDOR-$DEVICE-armv7hl -m sdk-install -R zypper ref
 sb2 -t $VENDOR-$DEVICE-armv7hl -m sdk-install -R \
