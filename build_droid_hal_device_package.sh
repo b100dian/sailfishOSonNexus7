d53d250bc8efd2ce5369bf17db262cdc08f15811
Marko Koschak <marko.koschak@tisno.de>
2014-08-20 23:21:03 +0200

    Update create repo commands to put rpm in its own folder

--- build_droid_hal_device_package.sh
+++ build_droid_hal_device_package.sh
@@ -5,7 +5,7 @@
 # This script is supposed to be run in the Mer SDK environment
 checkMerSDK
 
-echo " Building the droid-hal-device package"
+echo " Building the droid-hal-device rpm package"
 cd $ANDROID_ROOT
 # THE COMMAND BELOW WILL FAIL. It's normal, carry on with the next one.
 # Explanation: force installing of build-requirements by specifying the
@@ -15,17 +15,27 @@ mb2 -t $VENDOR-$DEVICE-armv7hl -s rpm/droid-hal-device.inc build
 mb2 -t $VENDOR-$DEVICE-armv7hl -s rpm/droid-hal-$DEVICE.spec build
 
 echo "Create a local rpm repository"
-mkdir -p $ANDROID_ROOT/droid-local-repo/$DEVICE
-rm -f $ANDROID_ROOT/droid-local-repo/$DEVICE/droid-hal-*rpm
-mv RPMS/*${DEVICE}* $ANDROID_ROOT/droid-local-repo/$DEVICE
+PKG=droid-hal-$DEVICE
+mkdir -p $ANDROID_ROOT/droid-local-repo/$DEVICE/$PKG/
+rm -f $ANDROID_ROOT/droid-local-repo/$DEVICE/$PKG/*.rpm
+mv RPMS/*${DEVICE}* $ANDROID_ROOT/droid-local-repo/$DEVICE/$PKG
+
 createrepo $ANDROID_ROOT/droid-local-repo/$DEVICE
 
 echo "Add local RPM repo to Target"
 sb2 -t $VENDOR-$DEVICE-armv7hl -R -m sdk-install \
 ssu ar local-$DEVICE-hal file://$ANDROID_ROOT/droid-local-repo/$DEVICE
 
-echo "Package droid-hal-config into rpm package"
+echo "Building the droid-hal-config rpm package"
 mb2 -t $VENDOR-$DEVICE-armv7hl \
 -s hybris/droid-hal-configs/rpm/droid-hal-configs.spec \
 build
+
+PKG=droid-hal-configs
+mkdir -p $ANDROID_ROOT/droid-local-repo/$DEVICE/$PKG/
+rm -f $ANDROID_ROOT/droid-local-repo/$DEVICE/$PKG/*.rpm
+mv RPMS/*.rpm $ANDROID_ROOT/droid-local-repo/$DEVICE/$PKG
+
+createrepo $ANDROID_ROOT/droid-local-repo/$DEVICE
+sb2 -t $VENDOR-$DEVICE-armv7hl -m sdk-install -R zypper ref
 
