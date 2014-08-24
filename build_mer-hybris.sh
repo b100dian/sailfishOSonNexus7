47556ec51f7970f1ed379a4b2574ce5b0f5a253a
Marko Koschak <marko.koschak@tisno.de>
2014-08-24 22:19:55 +0200

    Split build_mer-hybris.sh in a setup and build part
    - load_mer-hybris.sh is used to download the android sources
    - build_mer-hybris.sh checks if the kernel config is suitable for mer and builds afterwards the stuff

--- build_mer-hybris.sh
+++ build_mer-hybris.sh
@@ -5,13 +5,11 @@
 # This script is supposed to be run in the HABuild SDK (Android build chroot)
 checkHABuildSDK
 
-mkdir -p $ANDROID_ROOT
 cd $ANDROID_ROOT
-repo init -u git://github.com/mer-hybris/android.git -b hybris-10.1
-repo sync
-source build/envsetup.sh
-export USE_CCACHE=1
-breakfast $DEVICE
-rm .repo/local_manifests/roomservice.xml
+
+# Check if kernel config is suitable for mer
+
+
+# Now build the kernel, initrd, bionic libc and other stuff needed for mer
 make -j4 hybris-hal
 
---
+++ load_mer-hybris.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+. common_functions
+
+# This script is supposed to be run in the HABuild SDK (Android build chroot)
+checkHABuildSDK
+
+mkdir -p $ANDROID_ROOT
+cd $ANDROID_ROOT
+repo init -u git://github.com/mer-hybris/android.git -b hybris-10.1
+repo sync
+source build/envsetup.sh
+export USE_CCACHE=1
+breakfast $DEVICE
+rm .repo/local_manifests/roomservice.xml
+
