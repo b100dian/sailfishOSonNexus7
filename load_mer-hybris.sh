6e87606bee6400ad4968287ca18a0cddfdb6df1b
Marko Koschak <marko.koschak@tisno.de>
2014-08-26 10:27:58 +0200

    Checkout repo only if it does not exists locally

--- load_mer-hybris.sh
+++ load_mer-hybris.sh
@@ -5,9 +5,15 @@
 # This script is supposed to be run in the HABuild SDK (Android build chroot)
 checkHABuildSDK
 
-mkdir -p $ANDROID_ROOT
-cd $ANDROID_ROOT
-repo init -u git://github.com/mer-hybris/android.git -b hybris-10.1
+# checkout repo only if it does not exists locally
+if [ ! -d $ANDROID_ROOT ]; then
+  mkdir -p $ANDROID_ROOT
+  cd $ANDROID_ROOT
+  repo init -u git://github.com/mer-hybris/android.git -b hybris-10.1
+else
+  cd $ANDROID_ROOT
+fi
+
 repo sync
 source build/envsetup.sh
 export USE_CCACHE=1
